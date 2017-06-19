88aff5a280c01b3633d951cc3abb9bf673e66580
puricamaykol <puricamaykol@gmail.com>
2017-06-18 21:03:07 -0400

    created aclass for payments

---
+++ Payment.js
@@ -0,0 +1,33 @@
+'use strict'
+
+module.exports = class Payment {
+	constructor(paymentParameters){
+		this.amount = paymentParameters.amount
+        this.description = paymentParameters.description
+        this.cardHolder = paymentParameters.cardHolder
+        this.cardHolderId = paymentParameters.cardHolderId
+        this.cardNumber = paymentParameters.cardNumber
+        this.cvc = paymentParameters.cvc
+        this.paymentParameters.expirationDate
+        this.statusId = paymentParameters.statusId
+        this.ip = paymentParameters.ip
+        this.orderNumber = paymentParameters.orderNumber
+        this.address = paymentParameters.address
+        this.city = paymentParameters.city
+        this.zipCode = paymentParameters.zipCode
+        this.state = paymentParameters.state
+        
+        if(this.validate()){
+        	var error = new Error('Invalid payment attrs');
+      		error.name = 'Invalid';
+      		return error;
+        }
+	}
+	/**
+	 * [validate description]
+	 * @return {[type]} [description]
+	 */
+	validate(){
+		return true;
+	}
+}
